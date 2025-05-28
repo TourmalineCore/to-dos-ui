d25b5641e2451f8e1bda2e374b785ec88c392ff3
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-05-28 10:35:36 +0500

    feat: add a Home page link to ToDos page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,6 +2,13 @@ export function HomePage() {
   return (
     <div className="Home">
       Hello World!
+      <br />
+      <a
+        href="/to-dos"
+        data-cy="to-dos-page-link"
+      >
+        Go to ToDos!!!
+      </a>
     </div>
   )
 }
