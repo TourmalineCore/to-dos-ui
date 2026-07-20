45981c7831e584fa0b6ccf5503e80799a3444ac1
Ekaterina <echekina@gmail.com>
2026-07-20 21:21:12 +0500

    feat: #515: markup and styling

--- src/pages/cards/sections/Card.tsx
+++ src/pages/cards/sections/Card.tsx
@@ -1,10 +1,27 @@
 import "./Card.css"
+import cardImg from '../../../assets/img/card-img.png'
+
 export function Card() {
   return (
     <div
       className="card"
       data-cy="card">
-      Card 1
+      <div className="card__content">
+        <div className="card__image-wrapper">
+          <img
+            className="card__image"
+            src={cardImg}
+          />
+        </div>
+        <h2 className="card__title">Ivanov Ivan</h2>
+        <h3 className="card__subtitle">Frontend developer</h3>
+        <button
+          className="card__button"
+          type="button"
+        >
+          Edit
+        </button>
+      </div>
     </div>
   )
 }
