f8eb743e6f9f19776b6339ee2811b9e158612fb1
Ekaterina <echekina@gmail.com>
2026-09-02 16:08:18 +0500

    test: #515: remove cy.wrap to fix viewport rendering

--- src/pages/to-dos/sections/list/ToDosContentSnapshotTest.cy.tsx
+++ src/pages/to-dos/sections/list/ToDosContentSnapshotTest.cy.tsx
@@ -20,18 +20,6 @@ describe(`ToDosContent Snapshot test`, () => {
     it(`Take the snapshot of ToDosContent ${viewport.width}x${viewport.height}`, () => {
       cy.viewport(viewport.width, viewport.height)
 
-      cy.wrap(
-        Cypress.automation(`remote:debugger:protocol`, {
-          command: `Emulation.setDeviceMetricsOverride`,
-          params: {
-            width: viewport.width,
-            height: viewport.height,
-            deviceScaleFactor: 1,
-            mobile: false,
-          },
-        }),
-      )
-
       mountComponent()
 
       cy
