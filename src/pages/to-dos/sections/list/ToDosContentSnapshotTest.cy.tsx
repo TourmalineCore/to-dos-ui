470619652762e4d62086e2ad1acdeb8b4a16f5dd
Ekaterina <echekina@gmail.com>
2026-09-14 16:36:14 +0500

    test: #515: remove Cypress.automation, waiting for fonts, capture viewport parameter to further add if needed

--- src/pages/to-dos/sections/list/ToDosContentSnapshotTest.cy.tsx
+++ src/pages/to-dos/sections/list/ToDosContentSnapshotTest.cy.tsx
@@ -20,29 +20,11 @@ describe(`ToDosContent Snapshot test`, () => {
     it(`Take the snapshot of ToDosContent ${viewport.width}x${viewport.height}`, () => {
       cy.viewport(viewport.width, viewport.height)
 
-      cy.wrap(
-        Cypress.automation(`remote:debugger:protocol`, {
-          command: `Emulation.setDeviceMetricsOverride`,
-          params: {
-            width: viewport.width,
-            height: viewport.height,
-            deviceScaleFactor: 1,
-            mobile: false,
-          },
-        }),
-      )
-
       mountComponent()
 
       cy
-        .window()
-        .then((win) => win.document.fonts.ready)
-
-      cy
         .get(`[data-cy="to-dos-content"]`)
-        .compareSnapshot(`/${viewport.width}`, {
-          capture: `viewport`,
-        })
+        .compareSnapshot(`/${viewport.width}`)
     })
   })
 })
